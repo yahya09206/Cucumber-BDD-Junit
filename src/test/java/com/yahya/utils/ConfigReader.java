4cd1af2a54b8ad30dd7f50321b4de763da7d1bf0
yahya09206 <yahyahussein91@gmail.com>
2026-03-09 11:21:22 -0700

    copied over util files from previous project

---
+++ src/test/java/com/yahya/utils/ConfigReader.java
@@ -0,0 +1,31 @@
+package com.yahya.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+
+        try {
+            //2 - Open file using FileInputStream (open file)
+            FileInputStream file = new FileInputStream("config.properties");
+            //3 - Load the "properties" object with "file" (load properties)
+            properties.load(file);
+
+            // close the file in memory
+            file.close();
+        } catch (IOException e ){
+            System.out.println("FILE NOT FOUND WITH GIVEN PATH!!!!");
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String keyword){
+
+        return properties.getProperty(keyword);
+    }
+}
---
+++ src/test/java/com/yahya/utils/Driver.java
@@ -0,0 +1,67 @@
+package com.yahya.utils;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    // Private constructor to remove access to object
+
+    private Driver(){
+    }
+
+    /**
+     * Driver is private so no access from outside of class
+     * Made it static so it can be used in a static method
+     */
+
+    private static WebDriver driver;
+
+
+    public static WebDriver getDriver(){
+
+        // check if driver is null
+        if (driver == null){
+
+            /**
+             * read browser type from config file so it doesn't get affected from any changes
+             */
+            String browserType = ConfigReader.getProperty("browser");
+
+            switch (browserType){
+
+                case "chrome":
+                   //WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                case "firefox":
+                    //WebDriverManager.firefoxdriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                    break;
+                default:
+                    System.out.println("NO DRIVER FOUND");
+
+            }
+
+        }
+
+        // return existing driver if it is not null
+        return driver;
+    }
+
+    public static void closeDriver(){
+
+        // if driver isn't null
+        if (driver != null){
+            // terminate the instance of the driver
+            driver.quit();
+            // assign driver back to null
+            driver = null;
+        }
+    }
+}
