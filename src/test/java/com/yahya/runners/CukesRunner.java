a43cbf82a94f6b085f2f82caf41a858617085dab
yahya09206 <yahyahussein91@gmail.com>
2026-03-09 11:34:21 -0700

    ran into blocker with cukes runner class

--- src/test/java/com/yahya/runners/CukesRunner.java
+++ src/test/java/com/yahya/runners/CukesRunner.java
@@ -1,16 +1,13 @@
 package com.yahya.runners;
 
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features") // Location of your feature files
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions") // Package for step definitions
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report.html")
 public class CukesRunner {
-
-
 }
