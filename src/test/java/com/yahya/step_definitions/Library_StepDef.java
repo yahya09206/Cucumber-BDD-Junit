a22274d199f016d8e3dea011be55207f84e7598f
yahya09206 <yahyahussein91@gmail.com>
2026-03-11 13:07:26 -0700

    set up step defs for login

---
+++ src/test/java/com/yahya/step_definitions/Library_StepDef.java
@@ -0,0 +1,29 @@
+package com.yahya.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Library_StepDef {
+
+    @Given("user is on the login page of the library application")
+    public void user_is_on_the_login_page_of_the_library_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see the dashboard")
+    public void user_should_see_the_dashboard() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -4,3 +4,9 @@ Feature: Library login feature
   As a user, I should be able to login with correct credentials to different accounts. And dashboard should be displayed
   Accounts are: librarian, student, admin
 
+
+  Scenario: Login as librarian
+    Given user is on the login page of the library application
+    When user enters librarian username
+    And user enters librarian password
+    Then user should see the dashboard
