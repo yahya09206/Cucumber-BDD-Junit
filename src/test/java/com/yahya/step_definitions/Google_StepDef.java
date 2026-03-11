dec94e6a4a3c19715e60c681cfb6d88fc4b0c662
yahya09206 <yahyahussein91@gmail.com>
2026-03-11 11:47:38 -0700

    setting up step def for google search

---
+++ src/test/java/com/yahya/step_definitions/Google_StepDef.java
@@ -0,0 +1,17 @@
+package com.yahya.step_definitions;
+
+import com.yahya.utils.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Google_StepDef {
+
+    @When("user is on the google search page")
+    public void user_is_on_the_google_search_page() {
+        Driver.getDriver().get("https://google.com");
+    }
+    @Then("user should see title is google")
+    public void user_should_see_title_is_google() {
+
+    }
+}
