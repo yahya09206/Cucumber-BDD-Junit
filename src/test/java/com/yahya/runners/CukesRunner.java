a3d042e249a6f12785ffe18eb3b93317873b6bf2
yahya09206 <yahyahussein91@gmail.com>
2026-03-11 12:22:57 -0700

    test is running from feature file

--- src/test/java/com/yahya/runners/CukesRunner.java
+++ src/test/java/com/yahya/runners/CukesRunner.java
@@ -5,10 +5,6 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import org.junit.platform.suite.api.*;
-
-import static io.cucumber.junit.platform.engine.Constants.*;
-
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
