1288168f3448a92273cf1154ec479898a7a975e0
yahya09206 <yahyahussein91@gmail.com>
2026-03-11 12:24:05 -0700

    test is passing

--- src/test/java/com/yahya/step_definitions/Google_StepDef.java
+++ src/test/java/com/yahya/step_definitions/Google_StepDef.java
@@ -3,6 +3,7 @@ package com.yahya.step_definitions;
 import com.yahya.utils.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class Google_StepDef {
 
@@ -13,5 +14,9 @@ public class Google_StepDef {
     @Then("user should see title is google")
     public void user_should_see_title_is_google() {
 
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Google";
+
+        Assertions.assertEquals(actualTitle, expectedTitle);
     }
 }
