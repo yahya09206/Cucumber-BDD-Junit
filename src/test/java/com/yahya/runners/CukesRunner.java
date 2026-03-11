07965b74ec1d3a6cc7c1f91c66fc04b8dacff460
yahya09206 <yahyahussein91@gmail.com>
2026-03-10 22:12:43 -0700

    added dependency for junit suite

--- src/test/java/com/yahya/runners/CukesRunner.java
+++ src/test/java/com/yahya/runners/CukesRunner.java
@@ -1,6 +1,11 @@
 package com.yahya.runners;
 
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
