992faaa0041e8e9856d68b06aa41d7a38d282f68
yahya09206 <yahyahussein91@gmail.com>
2026-03-12 12:12:40 -0700

    setting up step defs

--- src/test/java/com/yahya/step_definitions/Library_StepDef.java
+++ src/test/java/com/yahya/step_definitions/Library_StepDef.java
@@ -8,22 +8,18 @@ public class Library_StepDef {
 
     @Given("user is on the login page of the library application")
     public void user_is_on_the_login_page_of_the_library_application() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User is on the login page of the library application.");
     }
     @When("user enters librarian username")
     public void user_enters_librarian_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User enters librarian username.");
     }
     @When("user enters librarian password")
     public void user_enters_librarian_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User enters librarian password.");
     }
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        System.out.println("User sees the dashboard");
     }
 }
